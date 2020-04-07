9d6d8ff25a12ce9c38bd37c1749b1b8b40c6f6fa
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-04-07 23:24:22 +0300

    added test to check the availability of the login link

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,13 @@
 from .pages.main_page import MainPage
 
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
