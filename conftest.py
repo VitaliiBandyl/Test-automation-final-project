22d403115dc67cf314b352d98ab1da67511c4007
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-04-07 21:52:29 +0300

    Initial commit

---
+++ conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose test language")
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.quit()
---
+++ test_main_page.py
@@ -0,0 +1,5 @@
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
