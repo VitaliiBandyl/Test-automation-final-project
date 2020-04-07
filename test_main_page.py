fa669f8d482d5d54ca99467b66b7faf9acca655c
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-04-07 22:23:12 +0300

    Create func open login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,11 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser):
+    browser.get(link)
+    go_to_login_page(browser)
