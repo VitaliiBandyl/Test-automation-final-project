dd3afc751db64b3a27bd7f2cdbaab6953367f56b
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-04-07 23:12:01 +0300

    Remade into PageObject style

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,8 @@
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
