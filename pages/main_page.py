7481ba5a97235197ccc1b223e8ed15fc772e98b8
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-04-08 10:58:13 +0300

    created assert exception for find login link

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
 
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
