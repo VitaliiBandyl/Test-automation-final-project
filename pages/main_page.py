492c4f5bff7d833cfed8b70e419314b3f7a1a680
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-04-08 11:14:54 +0300

    created separate file for css-selectors

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,12 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
