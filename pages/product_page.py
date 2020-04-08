6f1ed46370abd0fa02754c28f306608890d42f3a
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-04-08 20:31:35 +0300

    added asserts

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,6 +3,27 @@ from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
+    def should_be_add_to_basket_btn(self):
+        assert self.is_element_present(*ProductPageLocators.BUTTON_ADD_TO_BASKET), "Add to basket button isn't presented"
+
     def add_product_to_basket(self):
-        add_item = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_item = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
         add_item.click()
+
+    def should_be_message_about_adding(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), (
+            "Product name is not presented")
+        assert self.is_element_present(*ProductPageLocators.MESSAGE_ABOUT_ADDING), (
+            "Message about adding is not presented")
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        message = self.browser.find_element(*ProductPageLocators.MESSAGE_ABOUT_ADDING).text
+        assert product_name in message, "No product name in the message"
+
+    def should_be_message_basket_total(self):
+        assert self.is_element_present(*ProductPageLocators.MESSAGE_BASKET_TOTAL), (
+            "Message basket total is not presented")
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), (
+            "Product price is not presented")
+        message_basket_total = self.browser.find_element(*ProductPageLocators.MESSAGE_BASKET_TOTAL).text
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        assert product_price in message_basket_total, "No product price in the message"
