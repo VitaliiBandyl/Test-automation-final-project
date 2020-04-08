025474b62a6f36873cf2721c35a43081318d93c6
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-04-08 14:06:07 +0300

    Added URL check after redirect to login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_should_see_login_link(browser):
@@ -13,3 +14,5 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
