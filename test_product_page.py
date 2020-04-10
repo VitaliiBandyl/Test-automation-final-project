86a7b20ff09688bb70c4ee482db5e82bc68507cb
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-04-10 15:36:54 +0300

    Added marks for tests which should review

--- test_product_page.py
+++ test_product_page.py
@@ -13,6 +13,7 @@ class TestLoginFromProductPage:
         page.open()
         page.should_be_login_link()
 
+    @pytest.mark.need_review
     def test_guest_can_go_to_login_page_on_product_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = ProductPage(browser, link)
@@ -30,6 +31,7 @@ class TestGuestAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
         page = ProductPage(browser, link)
@@ -40,6 +42,7 @@ class TestGuestAddToBasketFromProductPage:
         basket_page.should_not_be_product()
         basket_page.should_be_message_empty_basket()
 
+    @pytest.mark.need_review
     @pytest.mark.parametrize('offer_number', ["0", "1", "2", "3", "4", "5", "6",
                                               pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
     def test_guest_can_add_product_to_basket(self, browser, offer_number):
@@ -61,7 +64,7 @@ class TestGuestAddToBasketFromProductPage:
 
 @pytest.mark.add_to_basket_user
 class TestUserAddToBasketFromProductPage:
-    @pytest.fixture
+    @pytest.fixture(autouse=True)
     def setup(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
         page = LoginPage(browser, link)
@@ -71,7 +74,7 @@ class TestUserAddToBasketFromProductPage:
         page.register_new_user(email, password)
         page.should_be_authorized_user()
 
-    def test_user_cant_see_product_in_basket_opened_from_product_page(self, browser, setup):
+    def test_user_cant_see_product_in_basket_opened_from_product_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
         page = ProductPage(browser, link)
         page.open()
@@ -81,13 +84,14 @@ class TestUserAddToBasketFromProductPage:
         basket_page.should_not_be_product()
         basket_page.should_be_message_empty_basket()
 
-    def test_user_cant_see_success_message(self, browser, setup):
+    def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
 
-    def test_user_can_add_product_to_basket(self, browser, setup):
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser,
                            f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0")
         page.open()
